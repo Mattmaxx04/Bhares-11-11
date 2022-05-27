d0347c472100aba53f8d0dee889921b4e0ea4839
Matias <matiasquiroga.04@gmail.com>
2022-05-27 19:09:48 -0300

    Form JS
    Config

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
- function enviarFormulario(){
-     console.log('Enviando formulario...');
- }
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validateName(){
+    var name = document.getElementById("contact-name").value;
+    
+    if(name.length == 0){
+    nameError.innerHTML = "Por favor escribir nombre";
+    return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = "Por favor escribir nombre completo";
+    return false;
+    }
+    nameError.innerHTML = "Valido";
+    return true;
+    }
